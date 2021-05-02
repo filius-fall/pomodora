1d321faf81da3e2f183316d4e0ff3cedb63938ba
filius-fall <sreeram596@gmail.com>
2021-05-02 18:37:30 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'pomodora',
+    version = '0.1',
+    py_modules = ['pomodora'],
+    install_requires = ['Click'],
+    entry_points = '''
+        [console_scripts]
+        pomodora = pomodora:timer
+        pp = pomodora:csv_reader
+    
+     ''',
+)
